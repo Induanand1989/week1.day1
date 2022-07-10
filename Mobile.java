cfad187b5db7b850d812a84859fcd653017bdf10
Induanand1989 <109015993+Induanand1989@users.noreply.github.com>
2022-07-10 12:58:22 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Mobile {
+	private void makeCall() {
+		// TODO Auto-generated method stub
+
+	}
+	private void sendMsg() {
+		// TODO Auto-generated method stub
+
+	}
+	public static void main(String[] args) {
+		System.out.println("This is my mobile");
+	}
+
+}
